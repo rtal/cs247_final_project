5cc421098158d2da13b82445cc75e497b75695b4
hannawii <hannawii@stanford.edu>
2016-03-01 02:58:57 -0800

    added changes for preliminary frontend restructuring

--- meal-recommendations/client/controllers/home.js
+++ meal-recommendations/client/controllers/home.js
@@ -1,6 +1,9 @@
 datafiles = ["/json/vegan.json"/*, "/json/omnivore.json", "/json/vegetarian.json"*/];
 var menufile = datafiles[Math.floor(Math.random() * datafiles.length)];
 
+var recipesList = new Array();
+var ingredientsInPossessionList = new Array();
+
 var recipes = {};
 var ingredientsInPossession = {};
 
@@ -36,24 +39,31 @@ function parseData(recipeArray, ingredientsArray) {
 	}
 	// parse ingredients in possession
 	for (var i = 0; i < ingredientsArray.length; i++) {
-		var currentIngredient = ingredientsArray[i];
-		ingredientsInPossession[currentIngredient["name"]] = currentIngredient["quantity"]
+		var currentCategory = ingredientsArray[i];
+		var tempIngredients = currentCategory["items"];
+		for (var j = 0; j < tempIngredients.length; j++) {
+			var currentIngredient = tempIngredients[j];
+			ingredientsInPossession[currentIngredient["name"]] = currentIngredient["quantity"];
+		}
 	}
 
 	return [recipes, ingredientsInPossession];
 }
 
 function recipeSelected (desiredRecipe, ingredientsNeeded) {
-	for (var ingredient in desiredRecipe) {
-		var quantityNeeded = desiredRecipe[ingredient]["quantity"];
+	for (var ingredient in recipes[desiredRecipe]) {
+		var quantityNeeded = recipes[desiredRecipe][ingredient]["quantity"];
 		if (ingredient in ingredientsNeeded) {
 			ingredientsNeeded[ingredient]["quantity"] += quantityNeeded;
 		} else {
 			ingredientsNeeded[ingredient] = {};
+			ingredientsNeeded[ingredient]["recipes"] = [];
 			ingredientsNeeded[ingredient]["quantity"] = quantityNeeded;
 		}
-		ingredientsNeeded[ingredient]["img"] = desiredRecipe[ingredient]["img"];
+		ingredientsNeeded[ingredient]["recipes"].push(desiredRecipe);
+		ingredientsNeeded[ingredient]["img"] = recipes[desiredRecipe][ingredient]["img"];
 	}
+	console.log(ingredientsNeeded);
 }
 
 function finalizeShoppingList (ingredientsInPossession, ingredientsNeeded) {
@@ -65,74 +75,122 @@ function finalizeShoppingList (ingredientsInPossession, ingredientsNeeded) {
 		}
 		if (difference > 0) {
 			shoppingList[ingredient] = {};
+			shoppingList[ingredient]["recipes"] = [];
 			shoppingList[ingredient]["quantity"] = difference;
 			shoppingList[ingredient]["img"] = ingredientsNeeded[ingredient]["img"];
+			var associatedRecipes = ingredientsNeeded[ingredient]["recipes"]
+			for (var i = 0; i < associatedRecipes.length; i++) {
+				var recipe = associatedRecipes[i];
+				if (shoppingList[ingredient]["recipes"].indexOf(recipe) < 0) {
+					shoppingList[ingredient]["recipes"].push(recipe);
+				}
+			}
 		}
 	}
 	return shoppingList;
 }
 
 function recipeUnselected (unselectedRecipe, ingredientsNeeded) {
-	for (var ingredient in unselectedRecipe) {
-		ingredientsNeeded[ingredient]["quantity"] -= unselectedRecipe[ingredient];
+	console.log(unselectedRecipe);
+	for (var ingredient in recipes[unselectedRecipe]) {
+		ingredientsNeeded[ingredient]["quantity"] -= recipes[unselectedRecipe][ingredient]["quantity"];
+		if (ingredientsNeeded[ingredient]["quantity"] < 0) {
+			ingredientsNeeded[ingredient]["quantity"] = 0;
+		}
+		var recipesWanted = ingredientsNeeded[ingredient]["recipes"];
+		var index = recipesWanted.indexOf(unselectedRecipe);
+		if (index >= 0) {
+			recipesWanted.splice(index, 1);
+		}
 	}
+	console.log(ingredientsNeeded);
 }
 
 results = openRecipeFile(menufile);
-parsedResults = parseData(results[0], results[1]);
+recipesList = results[0]
+ingredientsInPossessionList = results[1];
+parsedResults = parseData(recipesList, ingredientsInPossessionList);
 recipes = parsedResults[0];
 ingredientsInPossession = parsedResults[1];
 Session.set("newIngredients", []);
 
 Template.items.helpers({
-	items: function() {
-		var items = new Array();
-
-		$.ajax({
-	        url: menufile,
-	        async: false,
-	        dataType: 'json',
-	        success: function(json) {
-	        	items = json["ingredients_in_possession"]
-	        }
-	    });
-
-	    return eval(items);
+	categories: function() {
+		return ingredientsInPossessionList;
 	}
 });
+
+// Template.category.helpers({
+// 	isItems: function(type) {
+// 		if (type == "items") {
+// 			return true;
+// 		}
+
+// 		return false;
+// 	}
+// });
+
+Template.category.events({
+	"click .chevron_toggleable": function(event) {
+		$(event.currentTarget).toggleClass('ion-chevron-down ion-chevron-up');
+	}
+})
 
 Template.meals.helpers({
 	meals: function() {
-		var meals = new Array();
-
-		$.ajax({
-	        url: menufile,
-	        async: false,
-	        dataType: 'json',
-	        success: function(json) {
-	        	meals = json["recipes"]
-	        }
-	    });
-
-	    return eval(meals);
+		return recipesList;
 	}
 });
 
-Template.meal.events({
-	"click td.add_meal": function (event) {
-		var meal = $(event.currentTarget).find('span').text();
-		recipeSelected(recipes[meal], ingredientsNeeded);
-		var shoppingList = finalizeShoppingList(ingredientsInPossession, ingredientsNeeded);
-		var newIngredients = []
-		for (var ingredient in shoppingList) {
-			newIngredients.push({"name": ingredient, "quantity": shoppingList[ingredient]["quantity"], "img": shoppingList[ingredient]["img"]});
-		}
-		Session.set("newIngredients", newIngredients);
+function resetShoppingList() {
+	var shoppingList = finalizeShoppingList(ingredientsInPossession, ingredientsNeeded);
+	var newIngredients = []
+	for (var ingredient in shoppingList) {
+		newIngredients.push({"name": ingredient, 
+							 "quantity": shoppingList[ingredient]["quantity"], 
+							 "img": shoppingList[ingredient]["img"],
+							 "recipes": shoppingList[ingredient]["recipes"]});
+	}
+	Session.set("newIngredients", newIngredients);
+	console.log(newIngredients);
+}
+
+Template.num_servings.events({
+	"click div.add-meal": function (event) {
+		var meal = $(event.currentTarget).siblings('span').text();
+		recipeSelected(meal, ingredientsNeeded);
+		resetShoppingList();
+		var currServings = $(event.currentTarget).siblings('div.num-servings').children('span');
+		var newNum = parseInt(currServings.text(), 10) + 1;
+		currServings.text(newNum);
+		event.stopPropagation();
+
+	},
+	"click div.remove-meal": function (event) {
+		var currServings = $(event.currentTarget).siblings('div.num-servings').children('span');
+		var newNum = parseInt(currServings.text(), 10) - 1;
+		if (newNum < 0) return;
+		currServings.text(newNum);
+		var meal = $(event.currentTarget).siblings('span').text();
+		recipeUnselected(meal, ingredientsNeeded);
+		resetShoppingList();
+		event.stopPropagation();
 	}
 });
 
 Template.list.helpers({
 	ingredients: function() {
 		return Session.get("newIngredients");
+	}
+});
+
+Template.recipe_info.helpers({
+	inPossession: function(name, quantity) {
+		if (name in ingredientsInPossession) {
+			if (ingredientsInPossession[name] >= quantity) {
+				return true;
+			}
+		}
+		return false;
 	}
 });
